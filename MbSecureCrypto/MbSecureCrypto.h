74eac71fc70563e225bb110850a3e00441ee2610
Maverick Bozo <mavbozo@pm.me>
2024-10-29 20:15:37 +0700

    Initial Commit

---
+++ MbSecureCrypto/MbSecureCrypto.h
@@ -0,0 +1,18 @@
+//
+//  MbSecureCrypto.h
+//  MbSecureCrypto
+//
+//  Created by Maverick Bozo on 29/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MbSecureCrypto.
+FOUNDATION_EXPORT double MbSecureCryptoVersionNumber;
+
+//! Project version string for MbSecureCrypto.
+FOUNDATION_EXPORT const unsigned char MbSecureCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MbSecureCrypto/PublicHeader.h>
+
+
